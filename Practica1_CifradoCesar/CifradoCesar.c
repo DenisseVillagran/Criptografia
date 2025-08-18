596201ee9e01ee7f82311b58a52a2d3ae04e1a83
Lesliee Sarahi Cruz Buenavista <143286669+LesliSara@users.noreply.github.com>
2025-08-17 21:17:42 -0600

    [MODIFIED] CifradoCesar.c
    We change the start of each line as minus

--- Practica1_CifradoCesar/CifradoCesar.c
+++ Practica1_CifradoCesar/CifradoCesar.c
@@ -1,155 +1,78 @@
-#include <stdio.h> 
-
-#include <stdlib.h> 
-
-#include <string.h> 
-
- 
-
-Char* encriptado(char* mensaje, int llave){ 
-
-    Int i; 
-
-    For (i=0; i<strlen(mensaje); i++){ 
-
-        If (mensaje[i]== 32) 
-
-            Continue; 
-
-        Else if (mensaje[i] >= 65 && mensaje[i] <= 90) 
-
-            Mensaje[i] = 65 + (mensaje[i] – 65 + llave) % 26; 
-
-        Else if (mensaje[i] >= 97 && mensaje[i] <= 122)  
-
-            Mensaje[i] = 97 + (mensaje[i] – 97 + llave) % 26; 
-
-    } 
-
-    Return mensaje; 
-
-} 
-
- 
-
-Char* descencriptado(char* mensaje, int llave){ 
-
-    Int i; 
-
-    For (i=0; i<strlen(mensaje); i++){ 
-
-        If (mensaje[i]== 32) 
-
-            Continue; 
-
-        Else if (mensaje[i] >= 65 && mensaje[i] <= 90) 
-
-            Mensaje[i] = 65 + (mensaje[i] – 65 – llave) % 26; 
-
-        Else if (mensaje[i] >= 97 && mensaje[i] <= 122)  
-
-            Mensaje[i] = 97 + (mensaje[i] – 97 – llave) % 26; 
-
-    } 
-
-    Return mensaje; 
-
-} 
-
- 
-
-Int main() 
-
-{ 
-
-    Char* mensaje; 
-
-    Int llave, opcion; 
-
-     
-
-    Mensaje = (char*) malloc(256 * sizeof(char));//Reserva de memoria dinámica 
-
-    If (¡mensaje) { 
-
-        Printf(“Error de memoria\n”); 
-
-        Return 1; 
-
-    } 
-
-     
-
-    Printf(“Ingresa tu mensaje: \n”); 
-
-    Fflush(stdin); 
-
-    Gets(mensaje); 
-
-     
-
-    Printf(“Ingresa el número de llave\n”); 
-
-    Scanf(“%i”, &llave); 
-
-    Do{ 
-
-        Printf(“Ingrese la opción que desee ejecutar:\n”); 
-
-        Printf(“1) Encriptar\n”); 
-
-        Printf(“2) Descencrpitar\n”); 
-
-        Printf(“3) Salir\n”); 
-
-        Scanf(“%d”, &opcion); 
-
-         
-
-        Switch(opcion){ 
-
-            Case 1: 
-
-                Mensaje=encriptado(mensaje, llave); 
-
-                Printf(“Su mensaje se encripto con éxito, es: \n”); 
-
-                Printf(“%s\n”, mensaje); 
-
-                Break; 
-
-            Case 2: 
-
-                Mensaje=descencriptado(mensaje, llave); 
-
-                Printf(“Su mensaje se descencriptó con éxito, es: \n”); 
-
-                Printf(“%s\n”, mensaje); 
-
-                Break;  
-
-            Case 3: 
-
-                Printf(“Saliendo…\n”); 
-
-                Break; 
-
-            Default: 
-
-                Printf(“Ingrese otra opción: \n”); 
-
-                Break; 
-
-        } 
-
-    } while (opcion!=3); 
-
-     
-
-    Free(mensaje); 
-
-     
-
-    Return 0; 
-
-} 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* encriptado(char* mensaje, int llave){
+    int i;
+    for (i=0; i<strlen(mensaje); i++){
+        if (mensaje[i]== 32)
+            continue;
+        else if (mensaje[i] >= 65 && mensaje[i] <= 90)
+            mensaje[i] = 65 + (mensaje[i] - 65 + llave) % 26;
+        else if (mensaje[i] >= 97 && mensaje[i] <= 122) 
+            mensaje[i] = 97 + (mensaje[i] - 97 + llave) % 26;
+    }
+    return mensaje;
+}
+
+char* descencriptado(char* mensaje, int llave){
+    int i;
+    for (i=0; i<strlen(mensaje); i++){
+        if (mensaje[i]== 32)
+            continue;
+        else if (mensaje[i] >= 65 && mensaje[i] <= 90)
+            mensaje[i] = 65 + (mensaje[i] - 65 - llave) % 26;
+        else if (mensaje[i] >= 97 && mensaje[i] <= 122) 
+            mensaje[i] = 97 + (mensaje[i] - 97 - llave) % 26;
+    }
+    return mensaje;
+}
+
+int main()
+{
+    char* mensaje;
+    int llave, opcion;
+    
+    mensaje = (char*) malloc(256 * sizeof(char));//Reserva de memoria dinámica
+    if (!mensaje) {
+        printf("Error de memoria\n");
+        return 1;
+    }
+    
+    printf("Ingresa tu mensaje: \n");
+    fflush(stdin);
+    gets(mensaje);
+    
+    printf("Ingresa el número de llave\n");
+    scanf("%i", &llave);
+    do{
+        printf("Ingrese la opción que desee ejecutar:\n");
+        printf("1) Encriptar\n");
+        printf("2) Descencrpitar\n");
+        printf("3) Salir\n");
+        scanf("%d", &opcion);
+        
+        switch(opcion){
+            case 1:
+                mensaje=encriptado(mensaje, llave);
+                printf("Su mensaje se encripto con éxito, es: \n");
+                printf("%s\n", mensaje);
+                break;
+            case 2:
+                mensaje=descencriptado(mensaje, llave);
+                printf("Su mensaje se descencriptó con éxito, es: \n");
+                printf("%s\n", mensaje);
+                break; 
+            case 3:
+                printf("Saliendo...\n");
+                break;
+            default:
+                printf("Ingrese otra opción: \n");
+                break;
+        }
+    } while (opcion!=3);
+    
+    free(mensaje);
+    
+    return 0;
+}
