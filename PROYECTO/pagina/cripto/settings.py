1a35154bde3e6dff9ac7b68d8fb824ab12ee7bed
Lesliee Sarahi <lesli.lscb@gmail.com>
2025-11-20 01:00:07 -0600

    [ADD] Parte de https

--- PROYECTO/pagina/cripto/settings.py
+++ PROYECTO/pagina/cripto/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "sslserver",
     "votacion.apps.VotacionConfig",
     "django.contrib.admin",
     "django.contrib.auth",
--- PROYECTO/pagina/manage.py
+++ PROYECTO/pagina/manage.py
@@ -2,8 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import ssl
 
-
+if not hasattr(ssl, 'wrap_socket'):
+    def dev_ssl_patch(sock, keyfile=None, certfile=None, server_side=False, ssl_version=ssl.PROTOCOL_TLS, **kwargs):
+        context = ssl.SSLContext(ssl_version)
+        context.load_cert_chain(certfile, keyfile)
+        return context.wrap_socket(sock, server_side=server_side)
+    
+    ssl.wrap_socket = dev_ssl_patch
+    
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cripto.settings")
