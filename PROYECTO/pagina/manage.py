644b3b8e217b2d8e297ecf5bbb2ce30e744ebb1f
Hector <hector@pc-bda-hbh.fi.unam>
2025-11-19 18:53:54 -0600

    [ADD] modificaciones en settings.py y manage.py para https

--- PROYECTO/pagina/manage.py
+++ PROYECTO/pagina/manage.py
@@ -2,7 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import ssl
 
+if not hasattr(ssl, 'wrap_socket'):
+    def dev_ssl_patch(sock, keyfile=None, certfile=None, server_side=False, ssl_version=ssl.PROTOCOL_TLS, **kwargs):
+        context = ssl.SSLContext(ssl_version)
+        context.load_cert_chain(certfile, keyfile)
+        return context.wrap_socket(sock, server_side=server_side)
+    
+    ssl.wrap_socket = dev_ssl_patch
 
 def main():
     """Run administrative tasks."""
